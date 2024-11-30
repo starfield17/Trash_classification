17f405c5c6bfc27ada785de3623dd5f92ff5d2d2
CYPRESSEN <149873042+ZYPRESSEN-AND-OSPREY@users.noreply.github.com>
2024-11-30 11:03:55 +0800

    Update require.sh

--- TorchVision/require.sh
+++ TorchVision/require.sh
@@ -3,6 +3,6 @@ conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/r/
 conda config --set show_channel_urls yes
 pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple
-pip install torch opencv-python numpy
+pip install torch opencv-python numpy torchvision
 #一眼丁真，鉴定为纯纯的trash
 
