7a608ebf1ea316a5080b76754dafdb96e3b55592
CYPRESSEN <haniwacake@163.com>
2024-12-21 00:08:17 +0800

    修改 add_to_systemd.sh

--- YOLO_model/deploy/add_to_systemd.sh
+++ YOLO_model/deploy/add_to_systemd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# inits_crpt.sh
+# add_to_systemd.sh
 # 用法:
-#   安装服务: bash inits_crpt.sh install "conda_env_name" "/path/to/script.py"
-#   移除服务: bash inits_crpt.sh remove "/path/to/script.py"
+#   安装服务: bash add_to_systemd.sh install "conda_env_name" "/path/to/script.py"
+#   移除服务: bash add_to_systemd.sh remove "/path/to/script.py"
 
 echo "==============================="
-echo "初始化启动脚本: inits_crpt.sh"
+echo "初始化启动脚本: add_to_systemd.sh"
 echo "==============================="
 
 # 检查是否至少有一个参数
